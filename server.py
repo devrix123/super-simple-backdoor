518d8551c07e02e3b5ceafc4d4fb17d842ebe9d5
DarulFebri <70104102+devrix123@users.noreply.github.com>
2022-04-13 10:29:19 +0000

    Update server.py

--- server.py
+++ server.py
@@ -8,13 +8,13 @@ soket=socket.socket() # buat objek soket
 host="192.168.100.167" # bisa dialokasikan
 port=9901 # bisa dialokasikan
 soket.bind((host,port))
-perintah ="1" # aku masih mencari tahu cara agar perintah ini dapat diubah ubah saat menjalankan program point 1
+perintah ="shutdown now" # aku masih mencari tahu cara agar perintah ini dapat diubah ubah saat menjalankan program. (point 1)
 
 soket.listen(50) # makin banyak makinbagus kan? hahaha :D
 
 def perulangan(): # terus terus mengulang sampai baterai latop habis :D
 	while True:
-		koneksi,alamat=soket.accept() # karena data yang dikembalikan berupa tuple.			print("Mematikan : ",alamat) # biar keren :D
+		koneksi,alamat=soket.accept() # karena data yang dikembalikan berupa tuple
 		print("Mematikan perangkat : ",alamat) # biar keren :D
 		koneksi.send(perintah.encode()) # melaju....
 
